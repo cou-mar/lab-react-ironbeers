5ce0b4fa2dd4fa477bc28475268a6b51f184a67a
Courtney Marasigan <coumardev@gmail.com>
2022-12-12 12:27:27 -0500

    fixed add new

--- src/pages/NewBeer.js
+++ src/pages/NewBeer.js
@@ -13,17 +13,18 @@ const NewBeer = () => {
         description: "",
         first_brewed: "",
         brewers_tips: "",
-        attenuation_leve: 0,
+        attenuation_level: 0,
         contributed_by: "",
     });
     
     const updateList = (event) => setNewBeer({
-        ...NewBeer, 
+        ...newBeer, 
         [event.target.name]: event.target.value
     });
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log(newBeer)
         axios.post('https://ih-beers-api2.herokuapp.com/beers/new', {
             name: newBeer.name,
             tagline: newBeer.tagline,
@@ -48,25 +49,25 @@ const NewBeer = () => {
                 <h3>Add New Beer</h3>
                 <form onSubmit={handleSubmit}>
                     <label>Name</label><br />
-                    <input name="name" value={newBeer.name} onChange={setNewBeer} />
+                    <input name="name" value={newBeer.name} onChange={updateList} />
                 <br />
                     <label>Tagline</label><br />
-                    <input name="tagline" value={newBeer.tagline} onChange={setNewBeer} />
+                    <input name="tagline" value={newBeer.tagline} onChange={updateList} />
                 <br />
                     <label>Description</label><br />
-                    <input name="description" value={newBeer.description} onChange={setNewBeer} />
+                    <input name="description" value={newBeer.description} onChange={updateList} />
                 <br />              
                     <label>First Brewed</label><br />
-                    <input name="first_brewed" value={newBeer.first_brewed} onChange={setNewBeer} />
+                    <input name="first_brewed" value={newBeer.first_brewed} onChange={updateList} />
                 <br />
                     <label>Brewer Tips</label><br />
-                    <input name="brewers_tips" value={newBeer.brewers_tips} onChange={setNewBeer} />
+                    <input name="brewers_tips" value={newBeer.brewers_tips} onChange={updateList} />
                 <br />
                     <label>Attenuation Level</label><br />
-                    <input name="attenuation_level" value={newBeer.attenuation_level} onChange={setNewBeer} />
+                    <input name="attenuation_level" value={newBeer.attenuation_level} onChange={updateList} />
                 <br />
                     <label>Contributed By</label><br />
-                    <input name="contributed_by" value={newBeer.contributed_by} onChange={setNewBeer} />
+                    <input name="contributed_by" value={newBeer.contributed_by} onChange={updateList} />
                 <br />
                     <button className="addNewBttn">List New Beer</button>
                 </form>
